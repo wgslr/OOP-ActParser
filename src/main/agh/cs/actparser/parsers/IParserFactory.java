df040da74e6926534fd456d0584d5826bda830ae
Wojciech Geisler <wojciech.geisler@gmail.com>
2017-12-18 23:02:23 +0100

    Add comments to IParserFactory

--- src/main/agh/cs/actparser/parsers/IParserFactory.java
+++ src/main/agh/cs/actparser/parsers/IParserFactory.java
@@ -5,6 +5,13 @@ import agh.cs.actparser.ElementKind;
 import java.util.List;
 
 public interface IParserFactory {
+
+    /**
+     * Creates parser capable of processing given element kind.
+     * @param kind Kind of document element to process
+     * @param bodyLines Content to be processed by the parser
+     * @return Parser matching given kind
+     */
     AbstractParser makeParser(ElementKind kind, List<String>
             bodyLines);
 }
