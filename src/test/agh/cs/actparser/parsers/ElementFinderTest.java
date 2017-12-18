492cf75c2327944323e083a58f2fe8365afe2528
Wojciech Geisler <wojciech.geisler@gmail.com>
2017-12-19 00:01:49 +0100

    fixup! Add test for ElementFinder

--- src/test/agh/cs/actparser/parsers/ElementFinderTest.java
+++ src/test/agh/cs/actparser/parsers/ElementFinderTest.java
@@ -5,27 +5,24 @@ import agh.cs.actparser.elements.AbstractElement;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ElementFinderTest {
 
-    @Test
-    public void ElementFinderCreatesElementForEveryLeadTest() {
-        IParserFactory pf = new IParserFactory() {
-            @Override
-            public AbstractParser makeParser(ElementKind kind, List<String>
-                    bodyLines) {
-                return new AbstractParser(bodyLines) {
-                    @Override
-                    public AbstractElement makeElement() {
-                        return null;
-                    }
-                };
-            }
-        };
+    // mock parser factory
+    IParserFactory pf = (kind, bodyLines) -> new AbstractParser(bodyLines) {
+        @Override
+        public AbstractElement makeElement() {
+            return null;
+        }
+    };
 
+
+    @Test
+    public void getElementsCreatesElementForEveryLeadTest() {
         List<String> inputLines = Arrays.asList("Art. 1. Title",
                                                 "Article one body",
                                                 "Art. 2. Title2",
@@ -34,9 +31,20 @@ public class ElementFinderTest {
                                                 "Art. 4.");
 
         ElementFinder testSubject = new ElementFinder(inputLines,
+                                                      // One step above Article
                                                       ElementKind.Chapter,
                                                       pf);
-        assertEquals(4, testSubject.getElements().size());
+        assertEquals(4, testSubject.getChildrenElements().size());
+    }
+
+    @Test
+    public void getElementsReturnsEmptyListOnEmptyInput(){
+        List<String> inputLines = Collections.emptyList();
+        ElementFinder testSubject = new ElementFinder(inputLines,
+                                                      ElementKind.Chapter,
+                                                      pf);
+
+        assertEquals(Collections.emptyList(), testSubject.getChildrenElements());
     }
 
 }
