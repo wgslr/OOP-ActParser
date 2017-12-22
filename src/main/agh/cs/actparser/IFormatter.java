d5e036f5c3165c348b9fbe689dfee0441b9faca8
Wojciech Geisler <wojciech.geisler@gmail.com>
2017-12-22 22:54:34 +0100

    Add PlaintextFormatter

--- src/main/agh/cs/actparser/IFormatter.java
+++ src/main/agh/cs/actparser/IFormatter.java
@@ -2,9 +2,10 @@ package agh.cs.actparser;
 
 import agh.cs.actparser.elements.AbstractElement;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface IFormatter {
 
-    void print(List<AbstractElement> elements);
+    void print(Collection<AbstractElement> elements);
+
 }
---
+++ src/main/agh/cs/actparser/PlaintextFormatter.java
@@ -0,0 +1,26 @@
+package agh.cs.actparser;
+
+import agh.cs.actparser.elements.AbstractElement;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PlaintextFormatter implements IFormatter {
+    @Override
+    public void print(Collection<AbstractElement> elements) {
+        elements.stream()
+                .forEach(e -> {
+                    System.out.println(
+                            formatElement(e)
+                    );
+                    print(e.getChildren().values());
+                });
+    }
+
+    private String formatElement(AbstractElement element) {
+        return element.toString() + (element.isHeaderInline() ? "" : "\n");
+    }
+
+}
